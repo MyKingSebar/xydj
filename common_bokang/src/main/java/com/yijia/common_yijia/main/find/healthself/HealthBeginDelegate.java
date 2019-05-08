ed6367df7d690994b914e87af3c52ecb3a678cdc
wangpanfeng <wangpanfeng@bokangzhixin.com>
2019-05-08 19:30:42 +0800

    健康自测 开始页面

---
+++ common_bokang/src/main/java/com/yijia/common_yijia/main/find/healthself/HealthBeginDelegate.java
@@ -0,0 +1,54 @@
+package com.yijia.common_yijia.main.find.healthself;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.latte.ec.R;
+import com.example.yijia.delegates.LatteDelegate;
+
+public class HealthBeginDelegate extends LatteDelegate {
+
+    @Override
+    public Object setLayout() {
+        return R.layout.delegate_health_begin;
+    }
+
+    @Override
+    public void onBindView(@Nullable Bundle savedInstanceState, @NonNull View rootView) {
+
+        initView(rootView);
+    }
+
+    @Override
+    public void onLazyInitView(@Nullable Bundle savedInstanceState) {
+        super.onLazyInitView(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // TODO: inflate a fragment view
+        View rootView = super.onCreateView(inflater, container, savedInstanceState);
+        return rootView;
+    }
+
+    private void initView(View rootView) {
+        TextView button = rootView.findViewById(R.id.health_self_begin_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+    }
+
+}
---
+++ common_bokang/src/main/java/com/yijia/common_yijia/main/find/healthself/HealthMainDelegate.java
@@ -0,0 +1,79 @@
+package com.yijia.common_yijia.main.find.healthself;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.AppCompatTextView;
+import android.support.v7.widget.CardView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.example.latte.ec.R;
+import com.example.latte.ec.R2;
+import com.example.yijia.delegates.LatteDelegate;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import butterknife.Unbinder;
+
+public class HealthMainDelegate extends LatteDelegate {
+
+    @BindView(R2.id.tv_back)
+    RelativeLayout tvBack;
+    @BindView(R2.id.tv_title)
+    AppCompatTextView tvTitle;
+    @BindView(R2.id.tv_save)
+    AppCompatTextView tvSave;
+    @BindView(R2.id.health_self_warn_text)
+    TextView healthSelfWarnText;
+    @BindView(R2.id.health_self_view_cardview)
+    CardView healthSelfViewCardview;
+    Unbinder unbinder;
+
+    @Override
+    public Object setLayout() {
+        return R.layout.delegate_health_seif;
+    }
+
+    @Override
+    public void onBindView(@Nullable Bundle savedInstanceState, @NonNull View rootView) {
+
+        initView();
+
+        getSupportDelegate().loadRootFragment(R.id.health_self_view_cardview, new HealthBeginDelegate());
+    }
+
+    @Override
+    public void onLazyInitView(@Nullable Bundle savedInstanceState) {
+        super.onLazyInitView(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = super.onCreateView(inflater, container, savedInstanceState);
+        unbinder = ButterKnife.bind(this, rootView);
+        return rootView;
+    }
+
+    private void initView() {
+        tvTitle.setText(R.string.health_seft_test);
+        tvSave.setVisibility(View.GONE);
+        healthSelfWarnText.setText(R.string.health_seft_main_warn);
+
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
+    }
+
+    @OnClick(R2.id.tv_back)
+    public void onViewClicked() {
+        getSupportDelegate().pop();
+    }
+}
